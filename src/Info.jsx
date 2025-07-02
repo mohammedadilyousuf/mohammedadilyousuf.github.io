bf7a9d271bdc08d917df80803839156c04d8d745
myousuf <myousuf@paypal.com>
2025-07-02 12:19:37 +0530

    Responsiveness

--- src/Info.jsx
+++ src/Info.jsx
@@ -15,32 +15,31 @@ function Info() {
     return (
         <Box component="main" className="info">
             {/* Left side for text content */}
-            <Box className="container" sx={{flex: 1, maxWidth: '60%'}}>
-                <Typography variant="h3" sx={{color: '#FFFFFF', fontWeight: 'bold'}}>
+            <Box className="container">
+                <Typography variant="h3" sx={{color: '#FFFFFF', fontWeight: 'bold', fontSize: {xs: '1.5rem', sm: '2rem', md: '3rem'}}}>
                     Hey there,
                 </Typography>
-                <Typography variant="h1" sx={{color: '#FFFFFF', fontWeight: 'bold'}}>
+                <Typography variant="h1" sx={{color: '#FFFFFF', fontWeight: 'bold', fontSize: {xs: '2rem', sm: '3rem', md: '6rem'}}}>
                     I'm Adil!
                 </Typography>
-                <Typography variant="h4" sx={{color: '#EDEDED', fontWeight: 'bold', marginBottom: '20px'}}>
+                <Typography variant="h4" sx={{color: '#EDEDED', fontWeight: 'bold', marginBottom: '20px', fontSize: {xs: '1.2rem', sm: '1.5rem', md: '2.125rem'}}}>
                     A full stack software engineer.
                 </Typography>
-                <Typography variant="h6" sx={{color: '#F5F5DC'}}>
+                <Typography variant="h6" sx={{color: '#F5F5DC', fontSize: {xs: '0.9rem', sm: '1rem', md: '1.25rem'}}}>
                     I'm an innovative problem-solver with a passion for developing secure and efficient solutions at scale for complex problems
                 </Typography>
-                <Typography variant="h6" sx={{color: '#F5F5DC', marginTop: '16px'}}>
+                <Typography variant="h6" sx={{color: '#F5F5DC', marginTop: '16px', fontSize: {xs: '0.9rem', sm: '1rem', md: '1.25rem'}}}>
                     I'm focused on building and enhancing Identity Platform at PayPal. With four years of industry experience,
-                    I have contributed to the platform that empowers millions of users globally,
+                    I have contributed to the platform that empowers billions of transactions and millions of users globally,
                     demonstrating my ability to solve complex problems and optimize systems for scalability and efficiency.
                 </Typography>
             </Box>
 
             {/* Right side for image */}
-            <Box className="profile-image" sx={{flex: 1, textAlign: 'center'}}>
+            <Box className="profile-image">
                 <img
                     src={profilePic}
                     alt="Profile"
-                    style={{width: '350px', height: '350px', borderRadius: '50%'}}
                 />
                 {/* Social Media Icons */}
                 <Box className="profiles">
@@ -71,7 +70,7 @@ function Info() {
                     <a href="mailto:adilyousuf1234@gmail.com" target="_blank">
                         <img
                             src={email}
-                            alt="X"
+                            alt="Email"
                             style={{width: '50px', height: '50px', borderRadius: '20%'}}/>
                     </a>
                 </Box>
